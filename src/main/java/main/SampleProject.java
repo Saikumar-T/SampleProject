a93fc28c8404fe6f11b8564ba1e363f32c6f2f0b
saikumar <hp@saikumar>
2019-04-01 16:06:55 +0530

    written add and print hello function successfully

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -1,10 +1,12 @@
+
 package main;
+import java.util.Scanner;
 
 public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
+		String c = "Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
@@ -12,7 +14,11 @@ public class SampleProject {
 		return c;
 	}
 	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
 		System.out.println(printHello());
-		System.out.println(add(23,45));
+		System.out.println(add(a,b));
+		
 	}
 }
